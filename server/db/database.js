580e5d07814a55e908ca75117b4c31f3e06b9f63
Aaron-Celia <card123er@gmail.com>
2023-02-24 13:28:52 -0500

    minor fixes, ready to deploy

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,6 @@
-const Sequelize = require('Sequelize');
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(`postgres://localhost:5432/acme_schools_db`, {
+const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 })
 
