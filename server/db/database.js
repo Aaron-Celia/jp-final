543ae2cd3b45db6cacbbbb494b8589863e0dda5a
Aaron-Celia <card123er@gmail.com>
2023-02-24 13:37:14 -0500

    ssl fix

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 })
 
 
-module.exports = db
+module.exports = db;
